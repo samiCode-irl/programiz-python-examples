520fe554edf0e78cb077b99b9883ce3aa00af6a2
thesamimian <30494757+thesamimian@users.noreply.github.com>
2020-05-04 04:32:09 +0500

    Made add_two_numbers.py

---
+++ Introduction/add_two_numbers.py
@@ -0,0 +1,8 @@
+# Python Program to Add Two Numbers
+# In this program, you will learn to add two numbers and
+# display it using print() function.
+
+num1 = 2.3
+num2 = 4
+
+print(float(num1) + float(num2))
