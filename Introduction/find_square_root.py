1e617b41b7276079f95ad59d1071821b7af163d7
thesamimian <30494757+thesamimian@users.noreply.github.com>
2020-05-04 04:56:55 +0500

    Made find_square_root

---
+++ Introduction/find_square_root.py
@@ -0,0 +1,8 @@
+# Python Program to Find the Square Root
+# In this program, you'll learn to find the square root of a number
+# using exponent operator and cmath module.
+
+import math
+num = 8
+
+print('Square root of %0.3f is: %0.3f' % (num, math.sqrt(num)))
